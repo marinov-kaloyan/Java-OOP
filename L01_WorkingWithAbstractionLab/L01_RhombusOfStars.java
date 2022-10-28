37b164c43e53d3517f699996621ac68bd9d14b03
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-28 11:04:38 +0300

    01. Rhombus of Stars

--- L01_WorkingWithAbstractionLab/L01_RhombusOfStars.java
+++ L01_WorkingWithAbstractionLab/L01_RhombusOfStars.java
@@ -14,24 +14,24 @@ public class L01_RhombusOfStars {
 
     private static void printTopPart(int input) {
         for (int row = 1; row <= input; row++) {
-            printRow(input,row);
+            printRow(input, row);
 
         }
     }
 
     private static void printBottomPart(int input) {
-        for (int row = input -1; row >=1 ; row--) {
-            printRow(input,row);
+        for (int row = input - 1; row >= 1; row--) {
+            printRow(input, row);
         }
     }
 
 
-    private static void printRow(int n , int row){
-        for (int space = 1; space <=n-row ; space++) {
+    private static void printRow(int n, int row) {
+        for (int space = 1; space <= n - row; space++) {
             System.out.print(" ");
 
         }
-        for (int star = 1; star <=row ; star++) {
+        for (int star = 1; star <= row; star++) {
             System.out.print("* ");
         }
         System.out.println();
