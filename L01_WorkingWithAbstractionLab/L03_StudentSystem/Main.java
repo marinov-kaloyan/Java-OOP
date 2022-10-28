85fed8af630817ec79ecbf55059dbec68060d203
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-28 12:07:46 +0300

    3. Student System

---
+++ L01_WorkingWithAbstractionLab/L03_StudentSystem/Main.java
@@ -0,0 +1,20 @@
+package L01_WorkingWithAbstractionLab.L03_StudentSystem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentSystem studentSystem = new StudentSystem();
+
+        String[] input = readLine(scanner);
+        while (!input[0].equals("Exit")) {
+            studentSystem.parseCommand(input);
+            input= readLine(scanner);
+
+        }
+    }
+    private static String[] readLine(Scanner scanner){
+        return scanner.nextLine().split("\\s+");
+    }
+}
