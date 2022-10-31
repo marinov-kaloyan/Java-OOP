8efe716944d3e80fffc9c03ae5a743cfac8b8c41
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-31 16:05:37 +0200

    02. Card Rank

---
+++ E01_WorkingWithAbstractionExercise/E02_CardRank/CardRank.java
@@ -0,0 +1,18 @@
+package E01_WorkingWithAbstractionExercise.E02_CardRank;
+
+public enum CardRank {
+   ACE,
+   TWO,
+   THREE,
+   FOUR,
+   FIVE,
+   SIX,
+   SEVEN,
+   EIGHT,
+   NINE,
+   TEN,
+    JACK,
+    QUEEN,
+    KING;
+
+}
