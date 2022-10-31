5ee825c991af12deff593dd4b32898e870dea4fe
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-31 16:17:29 +0200

    03. Cards With Power

---
+++ E01_WorkingWithAbstractionExercise/E03_CardsWithPower/Main.java
@@ -0,0 +1,14 @@
+package E01_WorkingWithAbstractionExercise.E03_CardsWithPower;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CardRank cardRank = CardRank.valueOf(scanner.nextLine());
+        CardSuit cardSuit = CardSuit.valueOf(scanner.nextLine());
+        int cardPower = cardRank.getPower()+cardSuit.getPower();
+
+        System.out.printf("Card name: %s of %s; Card power: %d",cardRank.name(),cardSuit.name(),cardPower);
+    }
+}
