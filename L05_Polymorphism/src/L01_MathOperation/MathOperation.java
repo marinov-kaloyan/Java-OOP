4ff083bc7de24ca78002110523b72c451e358672
Arraxiel <kaloyanmarinov93@gmail.com>
2022-12-03 18:54:45 +0200

    01. Math Operation

---
+++ L05_Polymorphism/src/L01_MathOperation/MathOperation.java
@@ -0,0 +1,13 @@
+package L01_MathOperation;
+
+public class MathOperation {
+    public int add(int first, int second){
+        return first + second;
+    }
+    public int add(int first, int second,int third){
+        return first + second+third;
+    }
+    public int add(int first, int second,int third, int fourth){
+        return first + second+third+fourth;
+    }
+}
