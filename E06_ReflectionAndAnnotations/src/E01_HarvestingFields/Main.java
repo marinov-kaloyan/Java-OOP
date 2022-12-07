f2a6bfd5bcd891951c1c5016b7ac2114befbf674
Kaloyan Marinov <kaloyanmarinov93@gmail.com>
2022-12-07 23:31:42 +0200

    01. Harvesting Fields

---
+++ E06_ReflectionAndAnnotations/src/E01_HarvestingFields/Main.java
@@ -0,0 +1,59 @@
+package E01_HarvestingFields;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Main {
+
+	private final static String PRINT = "%s %s %s%n";
+	private final static String END_COMMAND = "HARVEST";
+	private final static String PRIVATE = "private";
+	private final static String PROTECTED = "protected";
+	private final static String PUBLIC = "public";
+	private final static Scanner CONSOLE = new Scanner(System.in);
+	private final static Field[] declaredFields = RichSoilLand.class.getDeclaredFields();
+
+	public static void main(String[] args) {
+
+		String command;
+
+		while (!END_COMMAND.equals(command = CONSOLE.nextLine())) {
+
+			switch (command) {
+				case PRIVATE:
+					filterFieldsModifier(PRIVATE);
+					break;
+
+				case PROTECTED:
+					filterFieldsModifier(PROTECTED);
+					break;
+
+				case PUBLIC:
+					filterFieldsModifier(PUBLIC);
+					break;
+
+				case "all":
+					Arrays.stream(declaredFields).
+							forEach(getFieldConsumer());
+					break;
+			}
+		}
+
+		CONSOLE.close();
+	}
+
+	private static void filterFieldsModifier(String aPrivate) {
+
+		Arrays.stream(declaredFields).
+				filter(s -> Modifier.toString(s.getModifiers()).equals(aPrivate)).
+				forEach(getFieldConsumer());
+	}
+
+	private static Consumer<Field> getFieldConsumer() {
+
+		return s -> System.out.printf(PRINT, Modifier.toString(s.getModifiers()), s.getType().getSimpleName(), s.getName());
+	}
+}
