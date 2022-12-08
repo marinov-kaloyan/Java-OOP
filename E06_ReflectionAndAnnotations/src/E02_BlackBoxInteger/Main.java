75abbfb90dc6aa291e01351bc8b2c9ed36f50ca2
Kaloyan Marinov <kaloyanmarinov93@gmail.com>
2022-12-08 21:57:53 +0200

    02. Black Box Integer

---
+++ E06_ReflectionAndAnnotations/src/E02_BlackBoxInteger/Main.java
@@ -0,0 +1,46 @@
+package E02_BlackBoxInteger;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    private final static Scanner CONSOLE = new Scanner(System.in);
+    private final static String END_COMMAND = "END";
+    private final static Map<String, Method> blackBoxIntMethods =
+            Arrays.stream(BlackBoxInt.class.getDeclaredMethods()).
+                    peek(method -> method.setAccessible(true)).
+                    collect(Collectors.toMap(Method::getName, method -> method));
+
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+
+        final Constructor<?> constructor = BlackBoxInt.class.getDeclaredConstructor(int.class);
+        constructor.setAccessible(true);
+
+        BlackBoxInt blackBoxInt = (BlackBoxInt) constructor.newInstance(0);
+
+        Field innerValue = BlackBoxInt.class.getDeclaredField("innerValue");
+        innerValue.setAccessible(true);
+
+        String command;
+
+        while (!END_COMMAND.equals(command = CONSOLE.nextLine())) {
+            String[] attributes = command.split("_");
+            String methodFromConsole = attributes[0];
+            Integer value = Integer.parseInt(attributes[1]);
+
+            Method method = blackBoxIntMethods.get(methodFromConsole);
+            method.invoke(blackBoxInt, value);
+
+            System.out.println(innerValue.getInt(blackBoxInt));
+        }
+
+        CONSOLE.close();
+    }
+}
